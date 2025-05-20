c6635a5ead1db9c700daed56d0ee9a531d5e5c90
DolevSeren <dolevseren33@gmail.com>
2025-05-20 21:19:50 +0300

    xp_excersise

--- W2/D3/xp_exersisesW2D3.py
+++ W2/D3/xp_exersisesW2D3.py
@@ -1,77 +1,77 @@
 # EX_1
 
-# class Currency:
-#     def __init__(self, currency, amount):
-#         self.currency = currency
-#         self.amount = amount
-#
-#     def __str__(self):
-#         return f"{self.amount} {self.currency}s"
-#
-#     def __int__(self):
-#         return self.amount
-#
-#     def __repr__(self):
-#         return f'{self.amount} {self.currency}'
-#
-#     def __add__(self, other):
-#         if isinstance(other, int):
-#             return Currency(self.currency, self.amount + other)
-#         elif isinstance(other, Currency):
-#             if self.currency != other.currency:
-#                 raise TypeError(f"Cannot add between Currency type <{self.currency}> and <{other.currency}>")
-#             return Currency(self.currency, self.amount + other.amount)
-#         else:
-#             raise TypeError("Unsupported type for addition")
-#
-#     def __iadd__(self, other):
-#         if isinstance(other, Currency):
-#             if self.currency != other.currency:
-#                 raise TypeError(f"Cannot add {self.currency} to {other.currency}")
-#             self.amount += other.amount
-#         elif isinstance(other, int):
-#             self.amount += other
-#         else:
-#             raise TypeError("Cannot add between Currency type <dollar> and <shekel>")
-#         return self
-#
-#
-#
-#
-# c1 = Currency('dollar', 5)
-# c2 = Currency('dollar', 10)
-# c3 = Currency('shekel', 1)
-# c4 = Currency('shekel', 10)
-#
-# #the comment is the expected output
-# str(c1)
-# # '5 dollars'
-#
-# int(c1)
-# # 5
-#
-# repr(c1)
-# # '5 dollars'
-#
-# print(c1 + 5)
-# # # 10
-#
-# print(c1 + c2)
-# # 15
-#
-# print(c1)
-# # 5 dollars
-#
-# c1 += 5
-# print(c1)
-# # # 10 dollars
-#
-# c1 += c2
-# print(c1)
-# # 20 dollars
-#
-# print(c1 + c3)
-# # TypeError: Cannot add between Currency type <dollar> and <shekel>
+class Currency:
+    def __init__(self, currency, amount):
+        self.currency = currency
+        self.amount = amount
+
+    def __str__(self):
+        return f"{self.amount} {self.currency}s"
+
+    def __int__(self):
+        return self.amount
+
+    def __repr__(self):
+        return f'{self.amount} {self.currency}'
+
+    def __add__(self, other):
+        if isinstance(other, int):
+            return Currency(self.currency, self.amount + other)
+        elif isinstance(other, Currency):
+            if self.currency != other.currency:
+                raise TypeError(f"Cannot add between Currency type <{self.currency}> and <{other.currency}>")
+            return Currency(self.currency, self.amount + other.amount)
+        else:
+            raise TypeError("Unsupported type for addition")
+
+    def __iadd__(self, other):
+        if isinstance(other, Currency):
+            if self.currency != other.currency:
+                raise TypeError(f"Cannot add {self.currency} to {other.currency}")
+            self.amount += other.amount
+        elif isinstance(other, int):
+            self.amount += other
+        else:
+            raise TypeError("Cannot add between Currency type <dollar> and <shekel>")
+        return self
+
+
+
+
+c1 = Currency('dollar', 5)
+c2 = Currency('dollar', 10)
+c3 = Currency('shekel', 1)
+c4 = Currency('shekel', 10)
+
+#the comment is the expected output
+str(c1)
+# '5 dollars'
+
+int(c1)
+# 5
+
+repr(c1)
+# '5 dollars'
+
+print(c1 + 5)
+# # 10
+
+print(c1 + c2)
+# 15
+
+print(c1)
+# 5 dollars
+
+c1 += 5
+print(c1)
+# # 10 dollars
+
+c1 += c2
+print(c1)
+# 20 dollars
+
+print(c1 + c3)
+# TypeError: Cannot add between Currency type <dollar> and <shekel>
 
 
 import string
