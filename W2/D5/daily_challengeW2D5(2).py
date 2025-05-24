f8690af96f783aa4cb94aa5461e1516cdacbb437
DolevSeren <dolevseren33@gmail.com>
2025-05-24 15:16:53 +0300

    daily_chalenge

---
+++ W2/D5/daily_challengeW2D5(2).py
@@ -0,0 +1,15 @@
+# EX_1
+
+
+# Answer the following questions:
+#
+# class is blueprint or template for creating objects. id defines a set of attributes and behaviors that the created object will have.
+# What is a class? = class is blueprint or template for creating objects. id defines a set of attributes and behaviors that the created object will have.
+# What is an instance? = is a specific object created from a class it contains real values for the class attributes and can use the classes methods.
+# What is encapsulation? = is the concept of bundling data and methods that operate on that data into a single unit and restricting direct access to some of the objects components.
+# What is abstraction? = is the concept of hiding complex implementation details and showing only the essential features of an object.
+# What is inheritance? = allow a class to inherit attributes and methods from another class.
+# What is multiple inheritance? = is when a class inherits from more than one class.
+# What is polymorphism? = allows objects of different classes to be treated as an instances of the same class through a shared interface
+# What is method resolution order or MRO? = is the order in which python looks for a method in a hierarchy of classes especially in multiple inheritance
+# What is method resolution order or MRO? = class is blueprint or template for creating objects. id defines a set of attributes and behaviors that the created object will have.
